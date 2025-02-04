0129558b3319a9be89d0181d412dd2e2ebc8f213
Nykyforuk Vasyl <nikiforuk.vasya@gmail.com>
2025-02-04 16:36:56 +0200

    apdete controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,7 +3,7 @@ import { getUserInit } from '../services/user.js';
 import createHttpError from 'http-errors';
 
 export const getInitUserController = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.bodi;
 
   const userInit = await getUserInit(id);
 
