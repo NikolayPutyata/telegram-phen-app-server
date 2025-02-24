952fafd9cfe334a2a5b4e20f4903560ab2bacd09
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-24 11:58:54 +0200

    upd

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -1,9 +1,7 @@
 import { addFriendToUserService } from '../services/webhook.js';
 import { bot } from '../utils/initTgBot.js';
 
-// Обработка команды /start
 bot.start(async (ctx) => {
-  console.log('Команда /start:', ctx.message);
   const friendId = ctx.from.id;
   const firstName = ctx.from.first_name;
   const messageText = ctx.message?.text || '';
@@ -13,48 +11,17 @@ bot.start(async (ctx) => {
   await ctx.reply('Hello in Phenerium!');
 });
 
-bot.on('message', async (ctx) => {
-  const data = JSON.parse(ctx.message.text);
-  console.log('✅ Распарсенные данные:', data);
-
-  if (data.action === 'pay' && data.chat_id) {
-    console.log('💰 Отправляем счёт на chat_id:', data.chat_id);
-    await bot.telegram.sendInvoice(data.chat_id, {
-      title: data.title,
-      description: data.description,
-      payload: data.payload,
-      provider_token: '', // Вставь свой токен
-      currency: 'XTR',
-      prices: data.prices,
-    });
-    console.log('🧾 Счет отправлен!');
-  } else {
-    console.log("❌ Нет chat_id или action !== 'pay'");
-  }
-});
-
-// Обработка предпроверки оплаты
 bot.on('pre_checkout_query', async (ctx) => {
-  console.log('Предпроверка оплаты:', ctx.preCheckoutQuery);
-  try {
-    await ctx.answerPreCheckoutQuery(true);
-  } catch (error) {
-    console.error('Ошибка предпроверки:', error.message);
-    await ctx.answerPreCheckoutQuery(false, 'Ошибка при проверке оплаты');
-  }
+  await ctx.answerPreCheckoutQuery(true);
 });
 
-// Обработка успешной оплаты
 bot.on('successful_payment', async (ctx) => {
-  console.log('Успешная оплата:', ctx.message.successful_payment);
   await ctx.reply(
     `Спасибо за покупку "${ctx.message.successful_payment.invoice_payload}"!`,
   );
 });
 
-// Контроллер вебхука
 export const botController = async (req, res) => {
-  console.log('Получен запрос от Telegram:', req.body); // Лог всех обновлений
   await bot.handleUpdate(req.body);
   res.sendStatus(200);
 };
