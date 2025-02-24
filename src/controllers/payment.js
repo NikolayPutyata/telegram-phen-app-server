4c32ea625a3ff801629623018def6f9e7bcfc3af
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-24 13:53:09 +0200

    upd

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -1,29 +1,17 @@
 import {
   createStarInvoiceService,
   formTransactionService,
-  successPaymentService,
+  // successPaymentService,
 } from '../services/payments.js';
 
 export const paymentSuccessController = async (req, res) => {
   const data = req.body;
 
-  const transactionId = data.signature;
-  const sender = data.accountData[0].account;
-  const memo = data.memo;
+  console.log(data);
 
-  // якщо приходить отак
-  //   {
-  //     "signature": "4324321230",
-  //     "accountData": [
-  //         {"account": "4qEQnt1BXPgBGXVs3pcnspAAit9f44147bDvLUkveHRw"}
-  //     ],
-  //     "memo": "ORDER_65600001_2_32"
+  // const user = await successPaymentService(transactionId, sender, memo);
 
-  // }
-
-  const user = await successPaymentService(transactionId, sender, memo);
-
-  res.status(200).json({ id: user.id });
+  res.status(200);
 };
 
 export const formTransactionController = async (req, res) => {
