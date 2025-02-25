408c8dcb7da8b95a8f316845ba63d33d72822b47
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-25 18:42:23 +0200

    upd tasks router

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
 import { tasksCompletedController } from '../controllers/tasks.js';
+import { tasksCompletedSchema } from '../validation/tasks.js';
 
 const router = Router();
 
-router.post('/completed', tasksCompletedController);
+router.post(
+  '/completed',
+  validateBody(tasksCompletedSchema),
+  tasksCompletedController,
+);
 
 export default router;
