65cd41758fb6e049b4f8b5641e44cd21f69f9186
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-25 18:57:38 +0200

    upd payment router

--- src/routers/payment.js
+++ src/routers/payment.js
@@ -1,14 +1,33 @@
 import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import {
   createStarInvoiceController,
   formTransactionController,
   paymentSuccessController,
 } from '../controllers/payment.js';
+import {
+  paymentSuccessSchema,
+  formTransactionSchema,
+  createStarInvoiceSchema,
+} from '../validation/payment.js';
 
 const router = Router();
 
-router.post('/payment-success', paymentSuccessController);
-router.post('/form-transaction', formTransactionController);
-router.post('/create-star-invoice', createStarInvoiceController);
+router.post(
+  '/payment-success',
+  validateBody(paymentSuccessSchema),
+  ctrlWrapper(paymentSuccessController),
+);
+router.post(
+  '/form-transaction',
+  validateBody(formTransactionSchema),
+  ctrlWrapper(formTransactionController),
+);
+router.post(
+  '/create-star-invoice',
+  validateBody(createStarInvoiceSchema),
+  ctrlWrapper(createStarInvoiceController),
+);
 
 export default router;
