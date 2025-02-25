68fb89316b297d22a7a3161669056c3e05e16669
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-25 18:58:59 +0200

    upd Webhook router

--- src/routers/webhook.js
+++ src/routers/webhook.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import { botController } from '../controllers/webhook.js';
+import { botWebhookSchema } from '../validation/webhook.js';
 
 const router = Router();
 
-router.post('/bot', ctrlWrapper(botController));
+router.post('/bot', validateBody(botWebhookSchema), ctrlWrapper(botController));
 
 export default router;
