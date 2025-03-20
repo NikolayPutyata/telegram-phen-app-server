5ace107f8ecfb53aecc6a1462e0ec29c5664e699
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-03-20 12:48:45 +0200

    upd

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -12,6 +12,9 @@ bot.start(async (ctx) => {
   const userId = messageText.split(' ')[1];
 
   console.log(userId);
+  console.log(userId.split(' '));
+  console.log(userId.split(' ')[1]);
+  console.log(userId.split(' ')[2]);
 
   await addFriendToUserService(userId, friendId, firstName);
   await ctx.reply(
